1534920595647976d3a59ac3fd49115786346f4d
MengChiaLee <77147836+MengChiaLee@users.noreply.github.com>
2021-03-31 17:20:29 +0800

    Update quiz2.js

--- quiz2.js
+++ quiz2.js
@@ -1,11 +1,11 @@
 <scrip>
-var tags = "§õª¾®¦";
+var tags = "Â§ÃµÂªÂ¾Â®Â¦";
 var dataUrl ="tps://api.flickr.com/services/feeds/photos\_public.gne?tags=" + tags +"&tagmode=any&format=json&per_page=400&jsoncallback=?"; 
 var data = $.getJSON(dataUrl); 
 data.done( function( msg ) {
   $.each(msg.item, function(i,item){
     $("#contain").html();
-    $("#contain").oppend($("<img/>").attr("scr", item.media.m))
+    $("#contain").oppend($("<img/>").attr("scr", item.media.m));
   });
 }); 
 data.fail( function( msg ) {
