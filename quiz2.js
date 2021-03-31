7990603132b5caa9a47b00da390a2d5a2f17eea0
MengChiaLee <77147836+MengChiaLee@users.noreply.github.com>
2021-03-31 17:31:16 +0800

    Update quiz2.js

--- quiz2.js
+++ quiz2.js
@@ -1,14 +1,14 @@
 
 var tags = "李知恩";
-var dataUrl ="https://api.flickr.com/services/feeds/photos\_public.gne?tags=" + tags +"&tagmode=any&format=json&per_page=400&jsoncallback=?"; 
-var data = $.getJSON(dataUrl); 
+var dataUrl = "https://api.flickr.com/services/feeds/photos\_public.gne?tags=" + tags
++"&tagmode=any&format=json&per_page=400&jsoncallback=?";
+var data = $.getJSON(dataUrl);
 data.done( function( msg ) {
-  $.each(msg.items, function(i,item){
-    $("#contain").html();
-    $("#contain").oppend($("<img/>").attr("scr", item.media.m));
-  });
-}); 
+$.each(msg.items,function(i,item){
+$("#contain").html;
+$("#contain").append($("<img/>").attr("src",item.media.m));
+});
+});
 data.fail( function( msg ) {
-  console.log(msg);
-  $("#contain").html("fail getting data");
-}); 
+console.log(msg);
+});
