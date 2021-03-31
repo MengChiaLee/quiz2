3a2d68a8db2376ce6ef50b1b13a447ce2f560fe6
MengChiaLee <77147836+MengChiaLee@users.noreply.github.com>
2021-03-31 17:23:04 +0800

    Update quiz2.js

--- quiz2.js
+++ quiz2.js
@@ -3,9 +3,9 @@ var tags = "§õª¾®¦";
 var dataUrl ="htttps://api.flickr.com/services/feeds/photos\_public.gne?tags=" + tags +"&tagmode=any&format=json&per_page=400&jsoncallback=?"; 
 var data = $.getJSON(dataUrl); 
 data.done( function( msg ) {
-  $.each(msg.item, function(i,item){
+  $.each(msg.items, function(i,item){
     $("#contain").html();
-    $("#contain").oppend($("<img/>").attr("scr", items.media.m));
+    $("#contain").oppend($("<img/>").attr("scr", item.media.m));
   });
 }); 
 data.fail( function( msg ) {
