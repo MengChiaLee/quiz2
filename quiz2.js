ef1b48444b265456049c716313f41573b5cf840e
MengChiaLee <77147836+MengChiaLee@users.noreply.github.com>
2021-03-31 17:19:00 +0800

    Add files via upload

---
+++ quiz2.js
@@ -0,0 +1,15 @@
+<scrip>
+var tags = "§õª¾®¦";
+var dataUrl ="tps://api.flickr.com/services/feeds/photos\_public.gne?tags=" + tags +"&tagmode=any&format=json&per_page=400&jsoncallback=?"; 
+var data = $.getJSON(dataUrl); 
+data.done( function( msg ) {
+  $.each(msg.item, function(i,item){
+    $("#contain").html();
+    $("#contain").oppend($("<img/>").attr("scr", item.media.m))
+  });
+}); 
+data.fail( function( msg ) {
+  console.log(msg);
+  $("#contain").html("fail getting data")
+}); 
+</scrip>
